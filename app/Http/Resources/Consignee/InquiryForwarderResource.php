bf74c0193e6c61952cb2d5dc7074b39a43fcf372
Wasim Rasheed <wasimrasheed3777@gmail.com>
2022-04-21 19:40:29 +0500

    heavy kam ho gya hai

--- app/Http/Resources/Consignee/InquiryForwarderResource.php
+++ app/Http/Resources/Consignee/InquiryForwarderResource.php
@@ -16,18 +16,18 @@ class InquiryForwarderResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'inquiry_id' => $this->id,
-            'volume' => $this->volume,
-            'weight' => $this->weight,
-            'date' => $this->seaFreight->date,
-            'notes' => $this->notes,
-            'is_dangerous' => $this->is_dangerous,
-            'loading_port_id' => $this->seaFreight->loading_port_id,
-            'destination_port_id' => $this->seaFreight->destination_port_id,
-            'loading_port' => $this->seaFreight->loadingPort->name ?? Null,
-            'destination_port' => $this->seaFreight->destinationPort->name ?? Null,
-            'commodity' => $this->commodity ?? null,
-            'containers' => InquiryContainerResource::collection($this->inquiryContainers)
+            'inquiry_id' => $this->inquiry->id,
+            'volume' => $this->inquiry->volume,
+            'weight' => $this->inquiry->weight,
+            'date' => $this->inquiry->seaFreight->date,
+            'notes' => $this->inquiry->notes,
+            'is_dangerous' => $this->inquiry->is_dangerous,
+            'loading_port_id' => $this->inquiry->seaFreight->loading_port_id,
+            'destination_port_id' => $this->inquiry->seaFreight->destination_port_id,
+            'loading_port' => $this->inquiry->seaFreight->loadingPort->name ?? Null,
+            'destination_port' => $this->inquiry->seaFreight->destinationPort->name ?? Null,
+            'commodity' => $this->inquiry->commodity ?? null,
+            'containers' => InquiryContainerResource::collection($this->inquiry->inquiryContainers)
         ];
     }
 }
