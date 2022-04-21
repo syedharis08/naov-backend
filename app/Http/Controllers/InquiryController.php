f485a5f5a8ec9b52d84efa731dde9f8ba1076764
Wasim Rasheed <wasimrasheed3777@gmail.com>
2022-04-21 20:25:25 +0500

    get accepted inquires

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -192,8 +192,8 @@ class InquiryController extends Controller
          $user = request()->user();
          $inquiries = $user->inquiries()->where('status','!=',0)->get();
          $shipperInquiries = $user->shipperInquiries()->where('status','!=',0)->get();
-         $mergedObject = array_merge($inquiries,$shipperInquiries);
-         return response()->json(['inquires'=> ConsigneeInquiryResource::collection($mergedObject)], Response::HTTP_OK);
+         $inquiries->merge($shipperInquiries);
+         return response()->json(['inquires'=> ConsigneeInquiryResource::collection($inquiries)], Response::HTTP_OK);
      }
     public function getForwarderAcceptedInquires()
     {
