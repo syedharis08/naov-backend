1fb12d85e96f7487620cdb3274fa7e687c57fbe1
Bilal Khalid <bilal.khalid@bitratic.com>
2022-04-24 23:55:33 +0500

    added adress details in ConsigneeInquiryResource

--- app/Http/Resources/Consignee/InquiryForwarderResource.php
+++ app/Http/Resources/Consignee/InquiryForwarderResource.php
@@ -40,9 +40,9 @@ class InquiryForwarderResource extends JsonResource
         if ($this->shipper_id) {
             $data['shipper_id'] = $this->shipper_id;
             $shipperAddress = $this->shipper->address;
-            $shipperAddress->country;
-            $shipperAddress->state;
-            $shipperAddress->city;
+            $shipperAddress->country();
+            $shipperAddress->state();
+            $shipperAddress->city();
             $data['shipper_address'] = $shipperAddress;
         }
         return $data;
