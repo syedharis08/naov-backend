539c95745fce06cb9e87109947c1957dcab95d81
syedharis08 <syedharis14@gmail.com>
2022-04-29 12:30:41 +0500

    removed dd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -155,7 +155,6 @@ class UserController extends Controller
     {
         $user = request()->user();
         $forwarder = $this->model::where('company_email', $request->company_email)->first();
-        dd(['forwarder' => $forwarder]);
         if (!$forwarder) {
             $forwarder = $this->model::create($request->all());
             $forwarder['url'] = 'localhost:3000/signup/service-provider/' . $forwarder->id;
