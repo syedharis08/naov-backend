1f25381c778e6354ced733978d41c7a377115548
Wasim Rasheed <wasimrasheed3777@gmail.com>
2022-05-08 21:02:15 +0500

    inquiry controller

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -247,7 +247,7 @@ class InquiryController extends Controller
         $inquiryForwarder = $user->inquiryForwarder()->where('inquiry_id', $id)->where('status', '=', 2)->first();
         $inquiryForwarderRate = $inquiryForwarder->inquiryForwarderRate()->where('status','1')->first();
         return response()->json(
-            ['inquiryRates' => InquiryForwarderRateResource::make($inquiryForwarderRate)],
+            ['inquiryRates' => InquiryForwarderRateResource::collection($inquiryForwarderRate)],
             Response::HTTP_OK
         );
     }
@@ -260,8 +260,8 @@ class InquiryController extends Controller
         $inquiryExtendedForwarderRate = $inquiryForwarder->inquiryExtendedForwarders()->where('rate_status',1)->first();
         $inquiryForwarderRate = $inquiryForwarder->inquiryForwarderRate()->where('status','1')->first();
         return response()->json(
-            ['inquiryRates' => InquiryForwarderRateResource::make($inquiryForwarderRate),
-                'extendedRates' => InquiryExtendedForwarderRateResource::make($inquiryExtendedForwarderRate)
+            ['inquiryRates' => InquiryForwarderRateResource::collection($inquiryForwarderRate),
+                'extendedRates' => InquiryExtendedForwarderRateResource::collection($inquiryExtendedForwarderRate)
             ],
             Response::HTTP_OK
         );
