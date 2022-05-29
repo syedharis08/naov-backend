7d83feb7f30ebe4890e70445340d7b28b5c4f086
syedharis08 <syedharis14@gmail.com>
2022-05-29 19:25:18 +0500

    email content updated

--- resources/views/emails/users/invitation.blade.php
+++ resources/views/emails/users/invitation.blade.php
@@ -1,15 +1,18 @@
 @component('mail::message')
-You are invited by {{ $user->invitation_user }} from {{ $user->invitation_company}} to manage their shipment on Naov!
-<br>
-<br>
-Naov is a global supply chain community where users and their service providers come together to easily move
-their goods around the world. Naov is focused in making mass shipping quick, easy, and accessible for everyone.
-Join {{ $user->invitation_company }} in this cause and together help us make a difference in the world.
+    You are invited by {{ $user->invitation_user }} from {{ $user->invitation_company }} to manage their shipment on
+    Naov!
+    <br>
+    <br>
+    Naov is a global supply chain management platform where supply chain companies can connect to their own freight
+    forwarders to easily manage
+    the movement of their goods around the world. Naov is focused in making mass shipping quick, easy, and accessible for
+    everyone.
+    Join {{ $user->invitation_company }} in this cause and together help us make a difference in the world.
 
-@component('mail::button', ['url' => $user->url])
-{{ $user->button }}
-@endcomponent
+    @component('mail::button', ['url' => $user->url])
+        {{ $user->button }}
+    @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
