b318cc6bcec803559eb5476b6dc38247ace14ebf
Wasim Rasheed <wasimrasheed3777@gmail.com>
2022-06-07 11:42:03 +0500

    inquiry accepted rate asseding order

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -158,7 +158,7 @@ class   InquiryController extends Controller
     function getConsigneeInquiryRate($id)
     {
         $inquiry = Inquiry::find($id);
-        $inquiryForwarderRates = $inquiry->inquiryForwarderRates()->where('inquiry_forwarders.status', 0)->orderBy('rate','ASC')->get();
+        $inquiryForwarderRates = $inquiry->inquiryForwarderRates()->where('inquiry_forwarders.status', 0)->orderBy('rate','DESC')->get();
         return response()->json(['inquiryRates' => InquiryForwarderRateResource::collection($inquiryForwarderRates)], Response::HTTP_OK);
     }
 
